8dd365222f5704653e675a88fa72899b185d2782
Piet de Vries <p.h.devries@gmail.com>
2017-10-15 09:39:09 +0200

    create key constants

--- app/src/main/java/com/rocdev/android/bakingtime/utils/JsonUtils.java
+++ app/src/main/java/com/rocdev/android/bakingtime/utils/JsonUtils.java
@@ -18,38 +18,53 @@ import java.util.List;
 
 class JsonUtils {
 
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_SERVINGS = "servings";
+    private static final String KEY_IMAGE = "image";
+    private static final String KEY_INGREDIENTS = "ingredients";
+    private static final String KEY_QUANTITY = "quantity";
+    private static final String KEY_MEASURE = "measure";
+    private static final String KEY_INGREDIENT = "ingredient";
+    private static final String KEY_STEPS = "steps";
+    private static final String KEY_SHORT_DESCRIPTION = "shortDescription";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_VIDEO_URL = "videoURL";
+    private static final String KEY_THUMB_URL = "thumbnailURL";
+
+
     static ArrayList<Recipe> extractRecipesFromJson(String jsonString) {
         ArrayList<Recipe> recipes = new ArrayList<>();
         try {
             JSONArray root = new JSONArray(jsonString);
             for (int i = 0; i < root.length(); i++) {
-                JSONObject recipeJs = root.getJSONObject(i);
-                int id = recipeJs.getInt("id");
-                String recipeName = recipeJs.getString("name");
-                int servings = recipeJs.getInt("servings");
-                String image = recipeJs.getString("image");
-                JSONArray ingredientsJs = recipeJs.getJSONArray("ingredients");
+                JSONObject recipeJO = root.getJSONObject(i);
+                int id = recipeJO.getInt(KEY_ID);
+                String recipeName = recipeJO.getString(KEY_NAME);
+                int servings = recipeJO.getInt(KEY_SERVINGS);
+                String image = recipeJO.getString(KEY_IMAGE);
+                JSONArray ingredientsJA = recipeJO.getJSONArray(KEY_INGREDIENTS);
                 List<Ingredient> ingredients = new ArrayList<>();
-                for (int j = 0; j < ingredientsJs.length(); j++) {
-                    JSONObject ingredientJs = ingredientsJs.getJSONObject(j);
-                    double quantity = ingredientJs.getDouble("quantity");
-                    String measure = ingredientJs.getString("measure");
-                    String ingredientName = ingredientJs.getString("ingredient");
+                for (int j = 0; j < ingredientsJA.length(); j++) {
+                    JSONObject ingredientJO = ingredientsJA.getJSONObject(j);
+                    double quantity = ingredientJO.getDouble(KEY_QUANTITY);
+                    String measure = ingredientJO.getString(KEY_MEASURE);
+                    String ingredientName = ingredientJO.getString(KEY_INGREDIENT);
                     ingredients.add(new Ingredient(ingredientName, measure, quantity));
                 }
                 List<Step> steps = new ArrayList<>();
-                JSONArray stepsJs = recipeJs.getJSONArray("steps");
-                for (int j = 0; j < stepsJs.length(); j++) {
-                    JSONObject stepJs = stepsJs.getJSONObject(j);
-                    int stepId = stepJs.getInt("id");
-                    String shortDescription = stepJs.getString("shortDescription");
-                    String description = stepJs.getString("description");
-                    String videoUrl = stepJs.getString("videoURL");
-                    String thumbnailURL = stepJs.getString("thumbnailURL");
+                JSONArray stepsJA = recipeJO.getJSONArray(KEY_STEPS);
+                for (int j = 0; j < stepsJA.length(); j++) {
+                    JSONObject stepJO = stepsJA.getJSONObject(j);
+                    int stepId = stepJO.getInt(KEY_ID);
+                    String shortDescription = stepJO.getString(KEY_SHORT_DESCRIPTION);
+                    String description = stepJO.getString(KEY_DESCRIPTION);
+                    String videoUrl = stepJO.getString(KEY_VIDEO_URL);
+                    String thumbnailURL = stepJO.getString(KEY_THUMB_URL);
                     steps.add(new Step(stepId, shortDescription, description, videoUrl,
                             thumbnailURL));
                 }
-                recipes.add(new Recipe(id, recipeName,servings, image, ingredients, steps));
+                recipes.add(new Recipe(id, recipeName, servings, image, ingredients, steps));
             }
         } catch (JSONException e) {
             e.printStackTrace();
