e4669806068bb2618eb8056897055d7c6bd7c220
Piet de Vries <p.h.devries@gmail.com>
2017-10-27 11:48:48 +0200

    very basic widget with textview that opens application on-click

--- app/src/main/java/com/rocdev/android/bakingtime/BakingTimeWidget.java
+++ app/src/main/java/com/rocdev/android/bakingtime/BakingTimeWidget.java
@@ -1,9 +1,13 @@
 package com.rocdev.android.bakingtime;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.rocdev.android.bakingtime.ui.MainActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -17,6 +21,9 @@ public class BakingTimeWidget extends AppWidgetProvider {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_time_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
